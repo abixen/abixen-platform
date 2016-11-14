8a3b3d97edfc109d40b3e24d6f20e3059665cbed
Konrad Wisniewski <konrad.k.wisniewski@gmail.com>
2016-11-14 23:59:13 +0100

    Added H2 support for databases connections in multivisualisation chart module.

--- abixen-platform-modules/src/main/java/com/abixen/platform/module/chart/service/impl/DatabaseH2ServiceImpl.java
+++ abixen-platform-modules/src/main/java/com/abixen/platform/module/chart/service/impl/DatabaseH2ServiceImpl.java
@@ -1,17 +1,3 @@
-/**
- * Copyright (c) 2010-present Abixen Systems. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
-
 package com.abixen.platform.module.chart.service.impl;
 
 import com.abixen.platform.module.chart.exception.DatabaseConnectionException;
@@ -26,12 +12,12 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
-
-@Service("POSTGRES")
-public class DatabasePostgresServiceImpl implements DatabaseService {
-
+/**
+ * Created by Konrad on 2016-11-14.
+ */
+@Service("H2")
+public class DatabaseH2ServiceImpl implements DatabaseService {
     private final Logger log = LoggerFactory.getLogger(DatabasePostgresServiceImpl.class);
 
 
@@ -44,25 +30,24 @@ public class DatabasePostgresServiceImpl implements DatabaseService {
     public Connection getConnection(DatabaseConnectionForm databaseConnectionForm) {
         try {
 
-            Class.forName("org.postgresql.Driver");
+            Class.forName("org.h2.Driver");
 
         } catch (ClassNotFoundException exception) {
 
-            log.error("Where is your PostgreSQL JDBC Driver? "
+            log.error("Where is your H2 JDBC Driver? "
                     + "Include in your library path!");
             throw new DatabaseConnectionException(exception.getMessage());
 
         }
 
-        log.info("PostgreSQL JDBC Driver Registered!");
+        log.info("H2 JDBC Driver Registered!");
 
         Connection connection;
 
         try {
 
             connection = DriverManager.getConnection(
-                    "jdbc:postgresql://" + databaseConnectionForm.getDatabaseHost() +
-                            ":" + databaseConnectionForm.getDatabasePort() + "/" +
+                    "jdbc:h2:mem:" +
                             databaseConnectionForm.getDatabaseName(), databaseConnectionForm.getUsername(),
                     databaseConnectionForm.getPassword());
 
@@ -109,6 +94,8 @@ public class DatabasePostgresServiceImpl implements DatabaseService {
 
     @Override
     public List<String> getTables(Connection connection) {
+        final int objectTypeIndex = 4;
+        final int objectNameIndex = 3;
 
         List<String> tables = new ArrayList<>();
 
@@ -117,8 +104,8 @@ public class DatabasePostgresServiceImpl implements DatabaseService {
             ResultSet rs = md.getTables(null, null, "%", null);
 
             while (rs.next()) {
-                if ("TABLE".equals(rs.getString(4)) || "VIEW".equals(rs.getString(4))) {
-                    tables.add(rs.getString(3));
+                if ("TABLE".equals(rs.getString(objectTypeIndex)) || "VIEW".equals(rs.getString(objectTypeIndex))) {
+                    tables.add(rs.getString(objectNameIndex));
                 }
             }
 
