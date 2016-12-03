2b370462ecd990a491da7700aa89b284600cfcb3
Mariusz Kumor <mariusz.kumor@abixen.com>
2016-12-03 19:46:07 +0100

    Adjusted the module types module in the admin panel to the Papa Style and new list grid. Resolved #124

--- abixen-platform-web-client/src/main/web/admin/modules/module-type/javascript/module-type-list.controller.js
+++ abixen-platform-web-client/src/main/web/admin/modules/module-type/javascript/module-type-list.controller.js
@@ -7,75 +7,74 @@
         .controller('ModuleTypeListController', ModuleTypeListController);
 
     ModuleTypeListController.$inject = [
-        '$scope',
-        '$http',
         '$log',
-        'uiGridConstants',
         'ModuleType',
-        'gridFilter',
         'applicationNavigationItems',
-        '$state',
         'toaster'
     ];
 
-    function ModuleTypeListController($scope, $http, $log, uiGridConstants, ModuleType, gridFilter, applicationNavigationItems, $state, toaster) {
-        $log.log('ModuleListController');
+    function ModuleTypeListController($log, ModuleType, applicationNavigationItems, toaster) {
+        $log.log('ModuleTypeListController');
 
-        angular.extend(this, new AbstractCrudListController($scope, $http, $log, uiGridConstants, ModuleType, gridFilter));
+        var moduleTypeList = this;
 
-        $scope.entityListGrid.columnDefs = [
-            {field: 'id', pinnedLeft: true, enableColumnResizing: false, enableFiltering: false, width: 50},
-            {field: 'name', pinnedLeft: true, width: 200},
-            {field: 'title', pinnedLeft: true, width: 200},
-            {field: 'serviceId', pinnedLeft: true, width: 200},
-            {field: 'initUrl', width: 400},
-            {field: 'description', width: 400},
-            {field: 'createdBy.username', name: 'Created By', width: 200},
+        angular.extend(moduleTypeList, new AbstractListGridController(ModuleType,
             {
-                field: 'createdDate',
-                width: 200,
-                cellFilter: "date:'" + platformParameters.formats.DATE_TIME_FORMAT + "'"
-            },
-            {field: 'lastModifiedBy.username', name: 'Last Modified By', width: 200},
-            {
-                field: 'lastModifiedDate',
-                width: 200,
-                cellFilter: "date:'" + platformParameters.formats.DATE_TIME_FORMAT + "'"
+                getTableColumns: getTableColumns
             }
-        ];
+        ));
 
-        $scope.query = {
-            and: [
+        moduleTypeList.reload = reload;
+        moduleTypeList.reloadAll = reloadAll;
+
+        updateNavigation();
+
+
+        function getTableColumns() {
+            return [
+                {field: 'id', pinnedLeft: true, enableColumnResizing: false, enableFiltering: false, width: 50},
+                {field: 'name', pinnedLeft: true, width: 200},
+                {field: 'title', pinnedLeft: true, width: 200},
+                {field: 'serviceId', pinnedLeft: true, width: 200},
+                {field: 'initUrl', width: 400},
+                {field: 'description', width: 400},
+                {field: 'createdBy.username', name: 'Created By', width: 200},
                 {
-                    name: 'title',
-                    operation: '=',
-                    value: 'Module View'
+                    field: 'createdDate',
+                    width: 200,
+                    cellFilter: "date:'" + platformParameters.formats.DATE_TIME_FORMAT + "'"
+                },
+                {field: 'lastModifiedBy.username', name: 'Last Modified By', width: 200},
+                {
+                    field: 'lastModifiedDate',
+                    width: 200,
+                    cellFilter: "date:'" + platformParameters.formats.DATE_TIME_FORMAT + "'"
                 }
+            ];
+        }
 
-            ]
-        };
+        function reload() {
+            ModuleType.reload({id: moduleTypeList.listGridConfig.selectedEntity.id})
+                .$promise
+                .then(onReload);
 
-        $scope.filterCriteria = {
-            page: 0,
-            size: 20,
-            sort: 'id,asc',
-            gridFilterParameters: []
-        };
-
-        $scope.reload = function () {
-            ModuleType.reload({id: $scope.selectedEntityId}, function (data) {
+            function onReload() {
                 toaster.pop(platformParameters.statusAlertTypes.SUCCESS, 'Success', 'Module\'s configuration has been reloaded.');
-            });
-        };
+            }
+        }
 
-        $scope.reloadAll = function () {
-            ModuleType.reloadAll({}, function (data) {
+        function reloadAll() {
+            ModuleType.reloadAll({})
+                .$promise
+                .then(onReloadAll);
+
+            function onReloadAll() {
                 toaster.pop(platformParameters.statusAlertTypes.SUCCESS, 'Success', 'All services\' configuration has been reloaded.');
-            });
-        };
+            }
+        }
 
-        applicationNavigationItems.clearTopbarItems();
-
-        $scope.read();
+        function updateNavigation() {
+            applicationNavigationItems.clearTopbarItems();
+        }
     }
 })();
