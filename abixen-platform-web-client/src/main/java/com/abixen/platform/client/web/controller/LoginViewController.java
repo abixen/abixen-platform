220bdaa1a4e512781504e4503e69644dff726c78
nagukothapalli <nagukothapalli9@gmail.com>
2016-12-22 02:13:07 +0530

    Migrated logger to log4j2 for the web client microservice. Resolved #236

--- abixen-platform-web-client/src/main/java/com/abixen/platform/client/web/controller/LoginViewController.java
+++ abixen-platform-web-client/src/main/java/com/abixen/platform/client/web/controller/LoginViewController.java
@@ -14,19 +14,15 @@
 
 package com.abixen.platform.client.web.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
+@Slf4j
 @Controller
 @RequestMapping("/login")
 public class LoginViewController {
-
-    private final Logger log = LoggerFactory.getLogger(LoginViewController.class);
-
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String renderLoginPage() {
