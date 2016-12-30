fef9e20d65c1af4b3e66a52eb7bbc9f98b07a6ea
aturski <adam.turski1@gmail.com>
2016-12-31 00:15:21 +0100

    Created domain classes for web content in the abixen platform web content module. Resolved #266

---
+++ abixen-platform-web-content-service/src/main/java/com/abixen/platform/service/webcontent/model/impl/WebContent.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2010-present Abixen Systems. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.abixen.platform.service.webcontent.model.impl;
+
+import com.abixen.platform.core.model.audit.AuditingModel;
+import com.abixen.platform.service.webcontent.model.enumtype.WebContentType;
+import com.abixen.platform.service.webcontent.model.web.WebContentWeb;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "web_content")
+@SequenceGenerator(sequenceName = "web_content_seq", name = "web_content_seq", allocationSize = 1)
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "type")
+public class WebContent extends AuditingModel implements WebContentWeb, Serializable {
+
+    private static final long serialVersionUID = 994392175080662107L;
+
+    public static final int TYPE_MAX_LENGHT = 255;
+    public static final int TITLE_MAX_LENGHT = 255;
+    public static final int CONTENT_MAX_LENGHT = 1000000;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "web_content_seq", strategy = GenerationType.SEQUENCE)
+    protected Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false, length = TYPE_MAX_LENGHT)
+    protected WebContentType type;
+
+    @Column(name = "title", nullable = false, length = TITLE_MAX_LENGHT)
+    protected String title;
+
+    @Column(name = "content", nullable = false, length = CONTENT_MAX_LENGHT, columnDefinition = "text")
+    protected String content;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public WebContentType getType() {
+        return type;
+    }
+
+    public void setType(WebContentType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
