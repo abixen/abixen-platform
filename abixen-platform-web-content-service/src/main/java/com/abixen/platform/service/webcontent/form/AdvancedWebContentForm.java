12ea00727be69eeb50f3a6b29368358e2379bc1f
aturski <adam.turski1@gmail.com>
2017-01-03 23:46:52 +0100

    Created a service class for the web content management in the abixen platform web content module. Resolved #271

---
+++ abixen-platform-web-content-service/src/main/java/com/abixen/platform/service/webcontent/form/AdvancedWebContentForm.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2010-present Abixen Systems. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.abixen.platform.service.webcontent.form;
+
+import com.abixen.platform.core.util.WebModelJsonSerialize;
+import com.abixen.platform.service.webcontent.model.impl.AdvancedWebContent;
+import com.abixen.platform.service.webcontent.model.web.StructureWeb;
+import com.abixen.platform.service.webcontent.model.web.TemplateWeb;
+import com.fasterxml.jackson.annotation.JsonView;
+
+import javax.validation.constraints.NotNull;
+
+public class AdvancedWebContentForm extends WebContentForm {
+
+    @NotNull
+    @JsonView(WebModelJsonSerialize.class)
+    private StructureWeb structure;
+
+    @NotNull
+    @JsonView(WebModelJsonSerialize.class)
+    private TemplateWeb template;
+
+    public AdvancedWebContentForm() {
+    }
+
+    public AdvancedWebContentForm(AdvancedWebContent advancedWebContent) {
+        super(advancedWebContent);
+        this.structure = advancedWebContent.getStructure();
+        this.template = advancedWebContent.getTemplate();
+    }
+
+    public StructureWeb getStructure() {
+        return structure;
+    }
+
+    public void setStructure(StructureWeb structure) {
+        this.structure = structure;
+    }
+
+    public TemplateWeb getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(TemplateWeb template) {
+        this.template = template;
+    }
+}
