9bac2f425c0f8b6bc54edfd9d4d780977dd3d289
Mariusz Kumor <mariusz.kumor@abixen.com>
2017-01-13 23:40:54 +0100

    Refactored JavaScript files of the multi visualisation of the admin side.

--- abixen-platform-business-intelligence-service/src/main/web/admin/businessintelligence/abixen/multi-visualization/javascript/query-builder.directive.js
+++ abixen-platform-business-intelligence-service/src/main/web/admin/businessintelligence/abixen/multi-visualization/javascript/query-builder.directive.js
@@ -1,14 +1,41 @@
-var multiVisualizationDirectives = angular.module('multiVisualizationDirectives', []);
+/**
+ * Copyright (c) 2010-present Abixen Systems. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 
-multiVisualizationDirectives.directive('queryBuilder', ['$compile', function ($compile) {
-    return {
-        restrict: 'E',
-        scope: {
-            group: '=',
-            fields: '='
-        },
-        templateUrl: '/admin/businessintelligence/abixen/multi-visualization/html/query-builder.html',
-        compile: function (element, attrs) {
+(function () {
+
+    'use strict';
+
+    angular
+        .module('multiVisualizationModule')
+        .directive('queryBuilder', queryBuilderDirective);
+
+
+    queryBuilderDirective.$inject = ['$compile'];
+
+    function queryBuilderDirective($compile) {
+        return {
+            restrict: 'E',
+            scope: {
+                group: '=',
+                fields: '='
+            },
+            templateUrl: '/admin/businessintelligence/abixen/multi-visualization/html/query-builder.html',
+            compile: compile
+        };
+
+
+        function compile(element, attrs) {
             var content, directive;
             content = element.contents().remove();
             return function (scope, element, attrs) {
@@ -16,14 +43,6 @@ multiVisualizationDirectives.directive('queryBuilder', ['$compile', function ($c
                     {name: 'AND'},
                     {name: 'OR'}
                 ];
-
-                /*scope.fields = [
-                 { name: 'Firstname' },
-                 { name: 'Lastname' },
-                 { name: 'Birthdate' },
-                 { name: 'City' },
-                 { name: 'Country' }
-                 ];*/
 
                 scope.conditions = [
                     {name: '='},
@@ -66,5 +85,6 @@ multiVisualizationDirectives.directive('queryBuilder', ['$compile', function ($c
                 }));
             }
         }
+
     }
-}]);
+})();
