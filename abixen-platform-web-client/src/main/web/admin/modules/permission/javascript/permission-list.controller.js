328f44fbc6501543ab971dea7fe0f2dcff39377e
Mariusz Kumor <mariusz.kumor@abixen.com>
2017-01-16 23:53:05 +0100

    Added missing name attributes to the permissions listgrid.

--- abixen-platform-web-client/src/main/web/admin/modules/permission/javascript/permission-list.controller.js
+++ abixen-platform-web-client/src/main/web/admin/modules/permission/javascript/permission-list.controller.js
@@ -43,20 +43,22 @@
 
         function getTableColumns() {
             return [
-                {field: 'id', pinnedLeft: true, enableColumnResizing: false, enableFiltering: false, width: 50},
-                {field: 'title', pinnedLeft: true, width: 200 /*filter: {term: 'cat|dog&!pink'}*/},
-                {field: 'description', enableSorting: false, width: 200/*, filter: {term: '(x1|x2)&(z1|z2)'}*/},
-                {field: 'permissionName', width: 200},
-                {field: 'permissionCategory', width: 200},
+                {field: 'id', name: 'Id', pinnedLeft: true, enableColumnResizing: false, enableFiltering: false, width: 50},
+                {field: 'title', name: 'Title', pinnedLeft: true, width: 200 /*filter: {term: 'cat|dog&!pink'}*/},
+                {field: 'description', name: 'Description', enableSorting: false, width: 200/*, filter: {term: '(x1|x2)&(z1|z2)'}*/},
+                {field: 'permissionName', name: 'Permission Name', width: 200},
+                {field: 'permissionCategory', name: 'Permission Category', width: 200},
                 {field: 'createdBy.username', name: 'Created By', width: 200},
                 {
                     field: 'createdDate',
+                    name: 'Created Date',
                     width: 200,
                     cellFilter: "date:'" + platformParameters.formats.DATE_TIME_FORMAT + "'"
                 },
                 {field: 'lastModifiedBy.username', name: 'Last Modified By', width: 200},
                 {
                     field: 'lastModifiedDate',
+                    name: 'Last Modified Date',
                     width: 200,
                     cellFilter: "date:'" + platformParameters.formats.DATE_TIME_FORMAT + "'"
                 }
