1438f4431bcdea68cadee27f516f10ae32959fc1
Mariusz Kumor <mariusz.kumor@abixen.com>
2017-01-28 23:58:03 +0100

    Error during a new page creation. Fixed #544

--- abixen-platform-core/src/main/java/com/abixen/platform/core/security/PlatformPermissionEvaluator.java
+++ abixen-platform-core/src/main/java/com/abixen/platform/core/security/PlatformPermissionEvaluator.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2010-present Abixen Systems. All rights reserved.
- *
+ * <p>
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- *
+ * <p>
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -58,7 +58,12 @@ public class PlatformPermissionEvaluator implements PermissionEvaluator {
         log.debug("targetDomainObject class: " + targetDomainObject.getClass());
         //TODO
         User user = userService.findUser(authentication.getName());
-        return securityService.hasUserPermissionToObject(user, PermissionName.valueOf((String) permission), (SecurableModel) targetDomainObject);
+
+        if (targetDomainObject instanceof String) {
+            return securityService.hasUserPermissionToClass(user, PermissionName.valueOf((String) permission), (String) targetDomainObject);
+        } else {
+            return securityService.hasUserPermissionToObject(user, PermissionName.valueOf((String) permission), (SecurableModel) targetDomainObject);
+        }
     }
 
     @Override
