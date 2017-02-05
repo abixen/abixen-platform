897fcac579c4cdef1adda5e7398ba6a332dfc2f1
Mariusz Kumor <mariusz.kumor@abixen.com>
2017-02-05 14:52:26 +0100

    A generic toString() method doesn't work properly and sometimes throws a recursive exception. Fixed #453

--- abixen-platform-core/src/main/java/com/abixen/platform/core/model/impl/AuditingModel.java
+++ abixen-platform-core/src/main/java/com/abixen/platform/core/model/impl/AuditingModel.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2010-present Abixen Systems. All rights reserved.
- *
+ * <p>
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- *
+ * <p>
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -54,7 +54,7 @@ public abstract class AuditingModel extends Model implements AuditingModelBase<U
     public abstract Long getId();
 
     public User getCreatedBy() {
-        if  (this instanceof User) {
+        if (this instanceof User) {
             if (!this.equals(createdBy)) {
                 return createdBy;
             } else {
@@ -112,14 +112,14 @@ public abstract class AuditingModel extends Model implements AuditingModelBase<U
         for (Field field : fields) {
             if (!Modifier.isStatic(field.getModifiers())) {
                 result.append("    ");
-                //try {
-                //    result.append(field.getName());
-                //    result.append(": ");
+                try {
+                    result.append(field.getName());
+                    result.append(": ");
                     //requires access to private field
-                //    result.append(field.get(this));
-                //} catch (IllegalAccessException e) {
-                //    e.printStackTrace();
-                //}
+                    result.append(field.get(this));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
                 result.append(newLine);
             }
         }
