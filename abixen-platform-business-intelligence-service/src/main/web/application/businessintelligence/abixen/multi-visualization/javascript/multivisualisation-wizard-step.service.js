19b4e722338f90e4e751ce4a3f617c3395ea15fe
Konrad Wisniewski <konrad.k.wisniewski@gmail.com>
2017-02-06 20:53:21 +0100

    Added translations of chart types on the first step of wizard. Resolved #583

--- abixen-platform-business-intelligence-service/src/main/web/application/businessintelligence/abixen/multi-visualization/javascript/multivisualisation-wizard-step.service.js
+++ abixen-platform-business-intelligence-service/src/main/web/application/businessintelligence/abixen/multi-visualization/javascript/multivisualisation-wizard-step.service.js
@@ -28,152 +28,152 @@
                 {
                     id: 0,
                     type: 'TABLE',
-                    name: 'Table view',
-                    description: 'Table view only. Use this module to presents table data without chart visualization'
+                    name: 'module.multivisualisation.configuration.chartType.table.name',
+                    description: 'module.multivisualisation.configuration.chartType.table.description'
                 },
                 {
                     id: 1,
                     type: 'LINE',
-                    name: 'Line chart view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.lineChart.name',
+                    description: 'module.multivisualisation.configuration.chartType.lineChart.description'
                 },
                 {
                     id: 2,
                     type: 'LINE_TABLE',
-                    name: 'Line chart with table view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.lineChartWithTable.name',
+                    description: 'module.multivisualisation.configuration.chartType.lineChartWithTable.description'
                 },
                 {
                     id: 3,
                     type: 'CUMULATIVE_LINE',
-                    name: 'Cumulative line chart view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.cumalativeLineChart.name',
+                    description: 'module.multivisualisation.configuration.chartType.cumalativeLineChart.description'
                 },
                 {
                     id: 4,
                     type: 'CUMULATIVE_LINE_TABLE',
-                    name: 'Cumulative line chart with table view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.cumalativeLineChartWithTable.name',
+                    description: 'module.multivisualisation.configuration.chartType.cumalativeLineChartWithTable.description'
                 },
                 {
                     id: 5,
                     type: 'STACKED_AREA',
-                    name: 'Stacked area chart view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.stackedAreaChart.name',
+                    description: 'module.multivisualisation.configuration.chartType.stackedAreaChart.description'
                 },
                 {
                     id: 6,
                     type: 'STACKED_AREA_TABLE',
-                    name: 'Stacked area chart with table view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.stackedAreaChartWithTable.name',
+                    description: 'module.multivisualisation.configuration.chartType.stackedAreaChartWithTable.description'
                 },
                 {
                     id: 7,
                     type: 'MULTI_COLUMN',
-                    name: 'Multi column chart view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.multiColumnChart.name',
+                    description: 'module.multivisualisation.configuration.chartType.multiColumnChart.description'
                 },
                 {
                     id: 8,
                     type: 'MULTI_COLUMN_TABLE',
-                    name: 'Multi column chart with table view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.multiColumnChartWithTable.name',
+                    description: 'module.multivisualisation.configuration.chartType.multiColumnChartWithTable.description'
                 },
                 {
                     id: 9,
                     type: 'DISCRETE_COLUMN',
-                    name: 'Discrete column chart view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.discreteColumnChart.name',
+                    description: 'module.multivisualisation.configuration.chartType.discreteColumnChart.description'
                 },
                 {
                     id: 10,
                     type: 'DISCRETE_COLUMN_TABLE',
-                    name: 'Discrete column chart with table view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.discreteColumnChartWithTable.name',
+                    description: 'module.multivisualisation.configuration.chartType.discreteColumnChartWithTable.description'
                 },
                 {
                     id: 11,
                     type: 'HISTORICAL_COLUMN',
-                    name: 'Historical column chart view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.historicalColumnChart.name',
+                    description: 'module.multivisualisation.configuration.chartType.historicalColumnChart.description'
                 },
                 {
                     id: 12,
                     type: 'HISTORICAL_COLUMN_TABLE',
-                    name: 'Historical column chart with table view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.historicalColumnChartWithTable.name',
+                    description: 'module.multivisualisation.configuration.chartType.historicalColumnChartWithTable.description'
                 },
                 {
                     id: 13,
                     type: 'MULTI_BAR',
-                    name: 'Multi bar chart view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.multiBarChart.name',
+                    description: 'module.multivisualisation.configuration.chartType.multiBarChart.description'
                 },
                 {
                     id: 14,
                     type: 'MULTI_BAR_TABLE',
-                    name: 'Multi bar chart with table view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.multiBarChartWithTable.name',
+                    description: 'module.multivisualisation.configuration.chartType.multiBarChartWithTable.description'
                 },
                 {
                     id: 15,
                     type: 'PIE',
-                    name: 'Pie chart view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.pieChart.name',
+                    description: 'module.multivisualisation.configuration.chartType.pieChart.description'
                 },
                 {
                     id: 16,
                     type: 'PIE_TABLE',
-                    name: 'Pie chart with table view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.pieChartWithTable.name',
+                    description: 'module.multivisualisation.configuration.chartType.pieChartWithTable.description'
                 },
                 {
                     id: 17,
                     type: 'SCATTER',
-                    name: 'Scatter chart view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.scatterChart.name',
+                    description: 'module.multivisualisation.configuration.chartType.scatterChart.description'
                 },
                 {
                     id: 18,
                     type: 'SCATTER_TABLE',
-                    name: 'Scatter chart with table view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.scatterChartWithTable.name',
+                    description: 'module.multivisualisation.configuration.chartType.scatterChartWithTable.description'
                 },
                 {
                     id: 19,
                     type: 'DONUT',
-                    name: 'Donut chart view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.donutChart.name',
+                    description: 'module.multivisualisation.configuration.chartType.donutChart.description'
                 },
                 {
                     id: 20,
                     type: 'DONUT_TABLE',
-                    name: 'Donut chart with table view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.donutChartWithTable.name',
+                    description: 'module.multivisualisation.configuration.chartType.donutChartWithTable.description'
                 },
                 {
                     id: 21,
                     type: 'LINE_WITH_FOCUS_CHART',
-                    name: 'Line Chart with focus mode',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.lineChartWithFocus.name',
+                    description: 'module.multivisualisation.configuration.chartType.lineChartWithFocus.description'
                 },
                 {
                     id: 22,
                     type: 'LINE_WITH_FOCUS_CHART_TABLE',
-                    name: 'Line Chart with focus mode with table view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.lineChartWithFocusAndTable.name',
+                    description: 'module.multivisualisation.configuration.chartType.lineChartWithFocusAndTable.description'
                 },
                 {
                     id: 23,
                     type: 'LINE_AND_BAR_WITH_FOCUS_CHART',
-                    name: 'Line and bar Chart with focus mode',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.lineAndBarChartWithFocus.name',
+                    description: 'module.multivisualisation.configuration.chartType.lineAndBarChartWithFocus.description'
                 },
                 {
                     id: 24,
                     type: 'LINE_AND_BAR_WITH_FOCUS_CHART_Table',
-                    name: 'Line and bar Chart with focus mode with table view',
-                    description: 'Use this module to represents data as a chart'
+                    name: 'module.multivisualisation.configuration.chartType.llineAndBarChartWithFocusAndTable.name',
+                    description: 'module.multivisualisation.configuration.chartType.llineAndBarChartWithFocusAndTable.description'
                 }
             ]
         }
