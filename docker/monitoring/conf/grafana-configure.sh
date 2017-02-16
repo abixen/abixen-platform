69cc58b1ccce066e7ebf37395e9b12a9622ec6aa
Tomasz Cholewa <tomasz@cloudowski.com>
2017-02-16 11:17:35 +0100

    Created basic dashboard in grafana. Resolved #662

--- docker/monitoring/conf/grafana-configure.sh
+++ docker/monitoring/conf/grafana-configure.sh
@@ -1,3 +1,5 @@
-curl -v  http://localhost:3000/api/datasources -d @datasource.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
-curl -v  http://localhost:3000/api/dashboards/db -d @dash.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
+sleep 20
+
+curl -v  http://grafana:3000/api/datasources -d @/conf/datasource.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
+curl -v  http://grafana:3000/api/dashboards/db -d @/conf/dash.json --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46YWRtaW4='
 
