bca5d45d7760e5a485006addd654a91008858d74
Mariusz Kumor <mariusz.kumor@abixen.com>
2017-04-29 18:21:07 +0200

    Created AuditingDto class and AuditingModelToAuditingDtoConverter class for functional microservices.

---
+++ abixen-platform-common/src/main/java/com/abixen/platform/common/converter/AuditingModelToAuditingDtoConverter.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2010-present Abixen Systems. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.abixen.platform.common.converter;
+
+
+import com.abixen.platform.common.dto.AuditingDto;
+import com.abixen.platform.common.model.audit.AuditingModel;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class AuditingModelToAuditingDtoConverter {
+
+    public AuditingDto convert(AuditingModel auditingModel, AuditingDto auditingDto) {
+
+        auditingDto
+                .setCreatedById(auditingModel.getCreatedById())
+                .setCreatedDate(auditingModel.getCreatedDate())
+                .setLastModifiedById(auditingModel.getLastModifiedById())
+                .setLastModifiedDate(auditingModel.getLastModifiedDate());
+
+        return auditingDto;
+    }
+}
---
+++ abixen-platform-common/src/main/java/com/abixen/platform/common/dto/AuditingDto.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2010-present Abixen Systems. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.abixen.platform.common.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+public abstract class AuditingDto {
+
+    private Long createdById;
+    private Date createdDate;
+    private Long lastModifiedById;
+    private Date lastModifiedDate;
+}
