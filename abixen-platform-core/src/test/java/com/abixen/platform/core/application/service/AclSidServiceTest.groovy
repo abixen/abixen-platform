d2f9feaa50d903b5c34f2571ec3bf3abe01150c2
mariuszkumor <mariusz.kumor@abixen.com>
2017-08-21 20:45:45 +0200

    Created AclSidServiceTest.

---
+++ abixen-platform-core/src/test/java/com/abixen/platform/core/application/service/AclSidServiceTest.groovy
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2010-present Abixen Systems. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.abixen.platform.core.application.service
+
+import com.abixen.platform.common.model.enumtype.AclSidType
+import com.abixen.platform.core.application.service.impl.AclSidServiceImpl
+import com.abixen.platform.core.domain.model.AclSid
+import com.abixen.platform.core.domain.model.AclSidBuilder
+import com.abixen.platform.core.domain.repository.custom.AclSidRepository
+import spock.lang.Specification
+
+
+class AclSidServiceTest extends Specification {
+
+    private AclSidService aclSidService;
+    private AclSidRepository aclSidRepository
+
+    void setup() {
+        aclSidRepository = Mock()
+        aclSidService = new AclSidServiceImpl(aclSidRepository)
+    }
+
+    void "should create AclSid"() {
+        given:
+        final AclSidType aclSidType = AclSidType.OWNER
+        final Long sidId = 1L
+
+        final AclSid aclSid = new AclSidBuilder()
+                .sidId(sidId)
+                .aclSidType(aclSidType)
+                .build();
+
+        aclSidRepository.save(aclSid) >> aclSid
+
+        when:
+        AclSid createdAclSid = aclSidService.createAclSid(aclSidType, sidId)
+
+        then:
+        createdAclSid.sidId == sidId
+        createdAclSid.sidType == aclSidType
+    }
+}
