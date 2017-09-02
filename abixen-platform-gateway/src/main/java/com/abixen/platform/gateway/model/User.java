d0a079637e90cfbc83cfbc8df7e8330521b6ce77
mariuszkumor <mariusz.kumor@abixen.com>
2017-09-02 11:41:43 +0200

    Removed unused fields from the model in Gateway.

--- abixen-platform-gateway/src/main/java/com/abixen/platform/gateway/model/User.java
+++ abixen-platform-gateway/src/main/java/com/abixen/platform/gateway/model/User.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2010-present Abixen Systems. All rights reserved.
- * <p>
+ *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * <p>
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -15,11 +15,9 @@
 package com.abixen.platform.gateway.model;
 
 import com.abixen.platform.common.model.Model;
-import com.abixen.platform.common.model.enumtype.UserGender;
 import com.abixen.platform.common.model.enumtype.UserLanguage;
 import com.abixen.platform.common.model.enumtype.UserState;
 
-import java.util.Date;
 import java.util.Set;
 
 
@@ -28,19 +26,12 @@ public class User extends Model {
     private Long id;
     private String username;
     private String password;
-    private String screenName;
     private String firstName;
-    private String middleName;
     private String lastName;
-    private String jobTitle;
-    private Date birthday;
-    private UserGender gender;
     private UserLanguage selectLanguage;
     private String avatarFileName;
-    private String registrationIp;
     private UserState state;
     private Set<Role> roles;
-    private String hashKey;
 
     @Override
     public Long getId() {
@@ -67,14 +58,6 @@ public class User extends Model {
         this.password = password;
     }
 
-    public String getScreenName() {
-        return screenName;
-    }
-
-    public void setScreenName(String screenName) {
-        this.screenName = screenName;
-    }
-
     public String getFirstName() {
         return firstName;
     }
@@ -83,44 +66,12 @@ public class User extends Model {
         this.firstName = firstName;
     }
 
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
     public String getLastName() {
         return lastName;
     }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
-
-    public Date getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
-
-    public UserGender getGender() {
-        return gender;
-    }
-
-    public void setGender(UserGender gender) {
-        this.gender = gender;
     }
 
     public UserLanguage getSelectedLanguage() {
@@ -139,14 +90,6 @@ public class User extends Model {
         this.avatarFileName = avatarFileName;
     }
 
-    public String getRegistrationIp() {
-        return registrationIp;
-    }
-
-    public void setRegistrationIp(String registrationIp) {
-        this.registrationIp = registrationIp;
-    }
-
     public UserState getState() {
         return state;
     }
@@ -161,14 +104,6 @@ public class User extends Model {
 
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
-    }
-
-    public String getHashKey() {
-        return hashKey;
-    }
-
-    public void setHashKey(String hashKey) {
-        this.hashKey = hashKey;
     }
 
 }
