a91c1a43f3e80e89899c9ca163ecb2530f8dd4ad
Konrad Wisniewski <konrad.k.wisniewski@gmail.com>
2017-09-07 13:24:45 +0200

    Tests for froms. Removed unused form

---
+++ abixen-platform-business-intelligence-service/src/test/java/com/abixen/platform/service/businessintelligence/multivisualisation/application/form/ChartConfigurationFormTest.groovy
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2010-present Abixen Systems. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.abixen.platform.service.businessintelligence.multivisualisation.application.form
+
+import com.abixen.platform.service.businessintelligence.multivisualisation.application.dto.ChartConfigurationDto
+import com.abixen.platform.service.businessintelligence.multivisualisation.application.dto.DataSetChartDto
+import com.abixen.platform.service.businessintelligence.multivisualisation.application.dto.DataSourceDto
+import com.abixen.platform.service.businessintelligence.multivisualisation.domain.model.enumtype.ChartType
+import spock.lang.Specification
+
+class ChartConfigurationFormTest extends Specification {
+
+    void "should build ChartConfigurationForm from ChartConfigurationDto"() {
+        given:
+        final DataSourceDto dataSource = [] as DataSourceDto
+        final DataSetChartDto dataSetChart = [] as DataSetChartDto
+
+        final ChartConfigurationDto chartConfigurationDto = new ChartConfigurationDto()
+                .setAxisXName("axisXName")
+                .setAxisYName("axisYName")
+                .setChartType(ChartType.CUMULATIVE_LINE)
+                .setDataSetChart(dataSetChart)
+                .setDataSource(dataSource)
+                .setFilter("filter")
+                .setId(1L)
+                .setModuleId(2L)
+
+        when:
+        final ChartConfigurationForm chartConfigurationForm = new ChartConfigurationForm(chartConfigurationDto)
+
+        then:
+        chartConfigurationForm != null
+        chartConfigurationForm.getId() == chartConfigurationDto.getId()
+        chartConfigurationForm.getModuleId() == chartConfigurationDto.getModuleId()
+        chartConfigurationForm.getFilter() == chartConfigurationDto.getFilter()
+        chartConfigurationForm.getAxisXName() == chartConfigurationDto.getAxisXName()
+        chartConfigurationForm.getAxisYName() == chartConfigurationDto.getAxisYName()
+        chartConfigurationForm.getChartType() == chartConfigurationDto.getChartType()
+        chartConfigurationForm.getDataSetChart() == chartConfigurationDto.getDataSetChart()
+        chartConfigurationForm.getDataSource() == chartConfigurationDto.getDataSource()
+
+        0 * _
+    }
+}
