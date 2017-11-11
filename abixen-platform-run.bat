dbd25c35010ea4cfd1b2b14fd73c84dd11b80236
sudaredd <sudaredd@gmail.com>
2017-11-11 14:14:06 -0500

    Changes to not to skip startups after build

--- abixen-platform-run.bat
+++ abixen-platform-run.bat
@@ -1,15 +1,10 @@
-echo staring mvn build
-
-IF EXIST "pom.xml" (
-  mvn clean package -DskipDocker
-  echo installed mvn
-) ELSE (
-    ECHO failed
-)
-start /b java -jar .\abixen-platform-eureka\target\abixen-platform-eureka.jar
-start /b java -jar .\abixen-platform-configuration\target\abixen-platform-configuration.jar
-start /b java -jar -Dspring.profiles.active=dev abixen-platform-gateway/target/abixen-platform-gateway.jar
-start /b java -jar -Dspring.profiles.active=dev abixen-platform-business-intelligence-service/target/abixen-platform-business-intelligence-service.jar
-start /b java -jar -Dspring.profiles.active=dev abixen-platform-web-content-service/target/abixen-platform-web-content-service.jar
-start /b java -jar -Dspring.profiles.active=dev abixen-platform-core/target/abixen-platform-core.jar
-start /b java -jar -Dspring.profiles.active=dev abixen-platform-web-client/target/abixen-platform.war
+ECHO staring mvn build
+call mvn clean package -DskipDocker
+ECHO "Maven build completed...starting applications"
+call start /b java -jar .\abixen-platform-eureka\target\abixen-platform-eureka.jar
+call start /b java -jar .\abixen-platform-configuration\target\abixen-platform-configuration.jar
+call start /b java -jar -Dspring.profiles.active=dev abixen-platform-gateway/target/abixen-platform-gateway.jar
+call start /b java -jar -Dspring.profiles.active=dev abixen-platform-business-intelligence-service/target/abixen-platform-business-intelligence-service.jar
+call start /b java -jar -Dspring.profiles.active=dev abixen-platform-web-content-service/target/abixen-platform-web-content-service.jar
+call start /b java -jar -Dspring.profiles.active=dev abixen-platform-core/target/abixen-platform-core.jar
+call start /b java -jar -Dspring.profiles.active=dev abixen-platform-web-client/target/abixen-platform.war
